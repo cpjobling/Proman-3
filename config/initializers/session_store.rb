069127fd1ee1fdf2a4b5fa2c2d037d3451944e0e
Chris Jobling <cpjobling@gmail.com>
2010-03-25 22:59:21 +0000

    Global replace Proman3 with proman

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -5,7 +5,7 @@
 # Make sure the secret is at least 30 characters and all random, 
 # no regular words or you'll be exposed to dictionary attacks.
 ActionController::Base.session = {
-  :key         => '_proman3_session',
+  :key         => '_proman_session',
   :secret      => '9e8853a49a0a752b742f539c506d27ff4707b671e82288e758b07ce0cefa0ffa8f82c7372925f815c49f6c68010a03609f6de7c002e53225e7f56f351cea6957'
 }
 
