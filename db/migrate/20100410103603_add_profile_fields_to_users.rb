91b1bfd56bee118398d3dad872871c67e203a381
Chris Jobling <cpjobling@gmail.com>
2010-04-10 15:34:09 +0100

    Added basic profile data (Name, Staff/Student ID, Nickname and Acceptance0 to user record

---
+++ db/migrate/20100410103603_add_profile_fields_to_users.rb
@@ -0,0 +1,21 @@
+class AddProfileFieldsToUsers < ActiveRecord::Migration
+  def self.up
+    add_column :users, :title, :string
+    add_column :users, :first_name, :string
+    add_column :users, :initials, :string
+    add_column :users, :last_name, :string
+    add_column :users, :known_as, :string
+    add_column :users, :staff_or_student_number, :integer
+    add_column :users, :has_accepted_terms_of_use, :boolean, :default => false
+  end
+
+  def self.down
+    remove_column :users, :has_accepted_terms_of_use
+    remove_column :users, :staff_or_student_number
+    remove_column :users, :known_as
+    remove_column :users, :last_name
+    remove_column :users, :initials
+    remove_column :users, :first_name
+    remove_column :users, :title
+  end
+end
