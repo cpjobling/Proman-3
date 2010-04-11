a59f3f18e40102508266ecb348c8104064abc521
Chris Jobling <cpjobling@gmail.com>
2010-04-11 15:59:17 +0100

    Added default top-level menu: home, projects, contact, about. Added sub-heading for site mission Added roots for :home, :projects, :contact, :about

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 ActionController::Routing::Routes.draw do |map|
   map.root :controller => "home", :action => "index"
+
+  map.about 'about', :conditions => { :method => :get}, :controller => 'about', :action => 'index'
+  map.home 'home', :conditions => { :method => :get}, :controller => 'home', :action => 'index'
+  map.contact 'contact', :conditions => { :method => :get}, :controller => 'contact', :action => 'index'
+  map.resources :projects
+  
   map.login '/login', :controller => "sessions", :action => "new"
   map.logout '/logout', :controller => "sessions", :action => "destroy"
 
